70ad06636f3ca217b36e662d3e9250be894c5ce2
tomas juarez <mokajua@gmail.com>
2021-11-20 22:24:20 -0300

    eventos

--- index.js
+++ index.js
@@ -15,76 +15,88 @@ class Credito{
   
 }
 
-let creditosIngresados = []
+let creditosIngresados = [];
 
-localStorage.setItem('creditos', JSON.stringify(creditosIngresados))
+
+const inputMonto = document.getElementById('monto');
+const inputCuotas = document.getElementById('cuotas');
+const buttonCredito =document.getElementById('btnLoad');
+const content = document.getElementById('content');
+const form = document.getElementById('my-form');
 
 const pedirCredito = () => {
-    let monto = parseInt(prompt('ingrese el monto del credito'));
-    let cuotas = parseInt(prompt('ingrese el numero de cuotas'));
+    
+    let monto = parseInt(inputMonto.value) ;
+    let cuotas = parseInt(inputCuotas.value);
     let interes = 1.21;
     let valorCuotas = (monto/cuotas) * interes;
     const credito = new Credito (monto ,cuotas);
-    creditosIngresados.push(credito)
+    
+    creditosIngresados.push(credito);
+    localStorage.setItem('creditos', JSON.stringify(creditosIngresados));
+    console.log(creditosIngresados);
+
     return credito;
 }
 
-const credito1 = pedirCredito();
-//const credito2 = pedirCredito();
-//const credito3 = pedirCredito();
-localStorage.setItem('creditos', JSON.stringify(creditosIngresados));
 
-let creditosAlmacenados = localStorage.getItem('creditos');
-console.log(creditosAlmacenados);
+const alertCredito = () =>{
+    const alert = document.createElement('div');
+    alert.className = "alert alert-danger";
+    alert.innerHTML = 'Los datos ingresados no son validos';
+    form.appendChild(alert);
+}
 
-let creditosAlmacenados2 = localStorage.getItem('creditos') //nuevo array para no manipular el original que no estoy usando para nada 
+
+const devolverCredito = () => {
+    
+    for (const Credito of creditosIngresados) {
+
+        
+         if ( (Credito.monto > 1000) && ((Credito.cuotas === 3) || (Credito.cuotas === 6) || (Credito.cuotas === 12) || (Credito.cuotas === 18))) 
+         {
+            
+            let tarjetasa = document.getElementById("div-card");
+            let div = document.createElement('div')
+                    
+                div.className = " card tarjetasa "
+    
+                div.innerHTML = `<h2>Mi credito</h2>
+                 <p>monto : $${Credito.monto}</p>
+                 <p>cuotas : ${Credito.cuotas}</p>
+                 <p>valor cuotas $${Credito.valorCuotas}</p>
+                 <button>Confirmar Credito</button>
+                 <hr>`;
+                tarjetasa.appendChild(div)
+            }
+            
+            else{ alertCredito()};
+    }
+
+}
+
+
+buttonCredito.onclick = (event) => {
+    event.preventDefault()
+    
+    pedirCredito();
+    devolverCredito();
+
+}
+
+
+
+/*let creditosAlmacenados = localStorage.getItem('creditos');
+console.log(creditosAlmacenados);*/
+
+/*let creditosAlmacenados2 = localStorage.getItem('creditos') //nuevo array para no manipular el original que no estoy usando para nada 
 if (creditosAlmacenados2 != null) {
     let nuevoArray = JSON.parse(creditosAlmacenados2)
     console.log(nuevoArray)
-}
-
-for (const Credito of creditosIngresados) {
-    
-    let div = document.createElement('div')
-        div.innerHTML = `<h2>Mi credito</h2>
-         <p>monto : $${Credito.monto}</p>
-         <p>cuotas : $${Credito.cuotas}</p>
-         <p>valor cuotas $${Credito.valorCuotas}</p>
-         <button>Confirmar Credito</button>
-         <hr>`;
-    document.body.appendChild(div)
+}*/
 
 
-    const ingresarCuotas = () => {
-        switch (Credito.cuotas) {
-            case 3:
-               
-                console.log('usted abonara 3 cuotas de $' + Credito.valorCuotas )
-                
-                return Credito.valorCuotas;
-            case 6:
-                console.log('usted abonara 6 cuotas de $' + Credito.valorCuotas )
-                
-                return Credito.valorCuotas;
-                
-            case 12:
-                console.log('usted abonara 12 cuotas de $' + Credito.valorCuotas )
-                
-                return Credito.valorCuotas;
-                
-            case 18:
-                console.log('usted abonara 18 cuotas de $' + Credito.valorCuotas )
-                
-                return Credito.valorCuotas;
-                
-            default:
-                break;
-        }
-            
-    }
-    
-     if (Credito.monto > 1000) {
-            ingresarCuotas();
-        }else{ alert('el monto de su credito es menor al minimo requerido')};
-}
+
+//eventos
+
 
