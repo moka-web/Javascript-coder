1aa7d8630335a0c07f846ce1c12856c71d3eec41
tomas juarez <mokajua@gmail.com>
2021-12-02 15:48:45 -0300

    intento jquery

--- index.js
+++ index.js
@@ -1,87 +1,107 @@
+$(document).ready( function(){
 
-class Credito{
+    class Credito{
     
-    constructor(monto , cuotas,){
-        this.monto = monto;
-        this.cuotas = cuotas;
-        this.interes = 1.21;
-        this.valorCuotas = (this.monto /this.cuotas) * this.interes;
-    }
-    
-    
-    modificarIntereses(intereses){
-        this.interes = intereses;
-    }
-  
-}
-
-let creditosIngresados = [];
-
-
-const inputMonto = document.getElementById('monto');
-const inputCuotas = document.getElementById('inputGroupSelect02');
-const buttonCredito =document.getElementById('btnLoad');
-const content = document.getElementById('content');
-const form = document.getElementById('my-form');
-
-const pedirCredito = () => {
-    
-    let monto = parseInt(inputMonto.value) ;
-    let cuotas = inputCuotas.value;
-    let interes = 1.21;
-    let valorCuotas = (monto/cuotas) * interes;
-    const credito = new Credito (monto ,cuotas);
-    
-    if ( (monto > 1000)){
+        constructor(monto , cuotas,){
+            this.monto = monto;
+            this.cuotas = cuotas;
+            this.interes = 1.21;
+            this.valorCuotas = (this.monto /this.cuotas) * this.interes;
+        }
         
-        creditosIngresados.push(credito);
-        localStorage.setItem('creditos', JSON.stringify(creditosIngresados));
-        console.log(creditosIngresados);
-        return credito;
+        
+        modificarIntereses(intereses){
+            this.interes = intereses;
+        }
+      
+    }
+
+    let creditosIngresados = [];
+
+    //const form = $('#user-form');
+
+    const inputMonto = document.getElementById('monto'); //equivale a document.getElementById()
+    const inputCuotas = document.getElementById('inputGroupSelect02');
+    const buttonCredito =document.getElementById('btnLoad');
+    const content = document.getElementById('content');
+    const form = document.getElementById('my-form');
+
+
+    const pedirCredito = () => {
     
-    }else{alertCredito()}
-     
-}
+        let monto = inputMonto.value ;
+        let cuotas = inputCuotas.value;
+        let interes = 1.21;
+        let valorCuotas = (monto/cuotas) * interes;
+        const credito = new Credito (monto ,cuotas);
+        
+        if ( (monto > 1000)){
+            
+            creditosIngresados.push(credito);
+            localStorage.setItem('creditos', JSON.stringify(creditosIngresados));
+            console.log(creditosIngresados);
+            return credito;
+        
+        }else{alertCredito()}
+         
+    }
 
 
-const alertCredito = () =>{
-    const alert = document.createElement('div');
-    alert.className = "alert alert-danger";
-    alert.innerHTML = 'ingrese un monto mayor a $1.000';
-    form.appendChild(alert);
-}
+    const alertCredito = () =>{
+        const alert = document.createElement('div');
+        alert.className = "alert alert-danger";
+        alert.innerHTML = 'ingrese un monto mayor a $1.000';
+        form.appendChild(alert);
+    }
 
-
-const devolverCredito = () => {
+    const devolverCredito = () => {
     
-    let tarjetasa = document.getElementById("div-card");
-    let div = document.createElement('div')
-    
-    for (const Credito of creditosIngresados) {
+        let tarjetasa = $("#div-card");
+        
+        for (const Credito of creditosIngresados) {
+                    
+            $(tarjetasa).append(
                 
-                div.className = " card tarjetasa "
+                    `<h2>Mi credito</h2>
+                    <p>monto : $${Credito.monto}</p>
+                    <p>cuotas : ${Credito.cuotas}</p>
+                    <p>valor cuotas $${Credito.valorCuotas}</p>
+                    <button>Confirmar Credito</button>
+                    <hr>`);          
+            
+        }
     
-                div.innerHTML = `<h2>Mi credito</h2>
-                 <p>monto : $${Credito.monto}</p>
-                 <p>cuotas : ${Credito.cuotas}</p>
-                 <p>valor cuotas $${Credito.valorCuotas}</p>
-                 <button>Confirmar Credito</button>
-                 <hr>`;
-                tarjetasa.appendChild(div)
         
     }
 
 
-}
-
-
-buttonCredito.onclick = (event) => {
-    event.preventDefault()
+    buttonCredito.onclick = (event) => {
+        event.preventDefault()
+        
+        pedirCredito();
+        devolverCredito();
     
-    pedirCredito();
-    devolverCredito();
+    }
 
-}
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
